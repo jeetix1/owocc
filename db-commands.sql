b98d78bec1b665ce00ce03d493ee3a663f2cfe88
Jeetix <jeetix@gmail.com>
2023-05-11 22:05:33 +0300

    adding timestamps to database

--- db-commands.sql
+++ db-commands.sql
@@ -5,3 +5,10 @@ CREATE TABLE `users` (
   `cookie_count` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- These queries will add the created_at and updated_at columns to existing users table.
+ALTER TABLE `users`
+ADD COLUMN `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE `users`
+ADD COLUMN `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
