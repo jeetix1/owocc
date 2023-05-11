5e9170ae8ade33fb0ba50f5f46ffd27c3a8bc26e
Jeetix <jeetix@gmail.com>
2023-05-12 00:48:58 +0300

    ollsiter bonus

--- public/script.js
+++ public/script.js
@@ -1,36 +1,39 @@
 $(document).ready(function() {
     var isJumping = false;
     var cookieCount = parseInt($('#cookieCount').text());
+    var clickCount = 0; // Track consecutive clicks
 
     $('#cookieBtn').click(function() {
         $.ajax({
             url: 'addCookie.php',
             success: function(data) {
                 $('#cookieCount').text(data);
-                
+
+                clickCount++; // Increment click count
+
                 if (!isJumping) {
                     isJumping = true;
                     $('#cookieBtn').addClass('jump');
-                    
+
                     setTimeout(function() {
                         $('#cookieBtn').removeClass('jump');
                         isJumping = false;
                     }, 500);
                 }
-                
+
                 var smallCookie = $('<img class="small-cookie" src="cookie-small.png">');
                 var x = Math.floor(Math.random() * ($(window).width() - smallCookie.width()));
                 var y = Math.floor(Math.random() * ($(window).height() - smallCookie.height()));
                 smallCookie.css({top: y, left: x});
                 $('body').append(smallCookie);
-                
+
                 setTimeout(function() {
                     smallCookie.fadeOut(500, function() {
                         $(this).remove();
                     });
                 }, 3000);
 
-                // Click event for smol cookies
+                // Click event for small cookies
                 smallCookie.click(function() {
                     $(this).fadeOut(500, function() {
                         $(this).remove();
@@ -38,13 +41,31 @@ $(document).ready(function() {
                         $('#cookieCount').text(cookieCount);
                     });
 
-                    // Trigger 10 more click events if the user clicks a smol cookie
+                    // Trigger 10 more click events if the user clicks a small cookie
                     for (var i = 0; i < 10; i++) {
                         setTimeout(function() {
                             $('#cookieBtn').trigger('click');
                         }, 100 * i);
                     }
                 });
+
+                // Check if the main cookie has been clicked 20 times in a row
+                if (clickCount === 20) {
+                    var gifContainer = $('<div id="gifContainer"><img src="ollister.gif"></div>');
+                    $('body').append(gifContainer);
+
+                    // Increase the amount of cookies the user gets for 20 seconds
+                    cookieCount += 10;
+                    $('#cookieCount').text(cookieCount);
+
+                    // Remove the gif container after 20 seconds
+                    setTimeout(function() {
+                        gifContainer.remove();
+                    }, 20000);
+
+                    // Reset click count
+                    clickCount = 0;
+                }
             }
         });
     });
