4a443db5ec98bd50e89ca4346bb1f5b6065b7d88
Jeetix <jeetix@gmail.com>
2023-05-12 17:38:48 +0300

    Just turned prod into my personal dev playground!

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
+require_once 'config-sensitive.php';
 
+/* // sample db-credentials
 $servername = "localhost";
 $dbname = "onex_owocc";
 $username_db = "onex_owocc";
 $password_db = "SuperSecret+Bonus";
+*/
