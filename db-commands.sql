23ce1611faa416b74847cbe6e78b4eaffcfdf190
Jeetix <jeetix@gmail.com>
2023-05-12 22:10:58 +0300

    db update

--- db-commands.sql
+++ db-commands.sql
@@ -12,3 +12,20 @@ ADD COLUMN `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
 ALTER TABLE `users`
 ADD COLUMN `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+
+CREATE TABLE cookie_titles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  min_count INT NOT NULL,
+  max_count INT NOT NULL,
+  custom_style VARCHAR(255)
+);
+
+INSERT INTO cookie_titles (title, min_count, max_count) VALUES
+('Dying!', 0, 9),
+('Starving', 10, 199),
+('Malnourished', 200, 349),
+('Struggling', 350, 620);
+-- There are almost 100 more that will be released publicly later!
+
